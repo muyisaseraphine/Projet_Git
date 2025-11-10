40b20b3136f839c609a9f5a6a9273aa25d33b0f2
muyisaseraphine <muyisaseraphine3@gmail.com>
2025-11-10 15:47:19 +0100

    Projet_git

---
+++ calcul.py
@@ -0,0 +1,11 @@
+def somma() :
+    """questa funzione restituisce la somma di due numeri inseriti dall'utente"""
+    a = float(input("Inserisci il primo numero: "))
+    b = float(input("Inserisci il secondo numero: "))
+    return a + b
+def produit() :
+    """questa funzione restituisce il prodotto di due numeri inseriti dall'utente"""
+    a = float(input("Inserisci il primo numero: "))
+    b = float(input("Inserisci il secondo numero: "))
+    return a * b
+    
